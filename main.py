06db5b7ec83ce40cf7ab025b3b8dabbda15d24d2
antoine <antoine.stuff@pm.me>
2021-06-20 15:11:44 +0200

    sensor works

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ spi = machine.SPI(1, baudrate=1000000)
 display = sh1106.SH1106_SPI(128, 64, machine.SPI(1), machine.Pin(5), machine.Pin(2), machine.Pin(4), False)
 display.rotate(180)
 
+# Sensor
+i2c = machine.I2C(0, scl=machine.Pin(9), sda=machine.Pin(8), freq=400000)
+
 
 def button_handler(pin):
     global btn_prev, btn_next, btn_up, btn_down, btn_prev_last, btn_next_last, btn_up_last, btn_down_last
@@ -55,3 +58,24 @@ btn_prev.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 btn_next.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 btn_up.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 btn_down.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+
+while True:
+    # Send the start conversion command to the SHT31
+    i2c.writeto(0x44, bytes([0x2C, 0x06]))
+    # wait for the conversion to complete
+    time.sleep(1)
+    # Read the data from the SHT31 containing
+    # the temperature (16-bits + CRC) and humidity (16bits + crc)
+    data = i2c.readfrom_mem(0x44, 0x00, 6)
+    # Convert the data
+    temp = data[0] * 256 + data[1]
+    cTemp = -45 + (175 * temp / 65535.0)
+    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+    # Output data to the terminal
+    print("Temperature in Celsius is : %.2fC" % cTemp)
+    print("Relative Humidity is : %.2f %%RH" % humidity)
+    # print the data to the OLED display
+    display.fill(0)
+    display.text("Temp:" + ("%.2f" % cTemp) + 'C', 20, 8)
+    display.text("Humid:" + ("%.2f" % humidity) + '%', 16, 24)
+    display.show()
